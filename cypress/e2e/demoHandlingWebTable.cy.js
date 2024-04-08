bdedadf6cf78c2107989959a5c2f6c0ff5878f90
AravindBalaji1990 <aravindbalaji.balaji7@gmail.com>
2024-04-08 20:46:37 +0300

    latest

--- cypress/e2e/demoHandlingWebTable.cy.js
+++ cypress/e2e/demoHandlingWebTable.cy.js
@@ -4,29 +4,24 @@
 describe('Handling Webtables', function() {
 it('Handling Webtables in the webpage',function() {
 cy.visit("https://rahulshettyacademy.com/AutomationPractice/")
-//first() -> represents the item which occurs in the first instance
-// scrollintoview -> it scrolls and navigates the page to the specific element
-// cy.get('#product').first().scrollIntoView();
-// cy.get('#product[name="courses"]').first().scrollIntoView();
-// cy.get("div.tableFixHead>table>tbody>tr").each(($data,index) => {
-//     const dataafter = $data.text()
-//     cy.get("div.tableFixHead>table>tbody>tr").find("td").then(function(datacolumn){
-//         cy.log(datacolumn)
-//     })
+// to print the valus from the tables
+// below item is for second table
+cy.get("div.tableFixHead>table>tbody>tr").each(($data,index) => {
+    const dataafter = $data.text()
+    cy.log('cehck the row', dataafter)
+    cy.get("div.tableFixHead>table>tbody>tr").find("td").then(function(datacolumn){
+        cy.log(datacolumn.text())
+    })
 
-// })
+})
 
-cy.get("tr td:nth-child(2)").each(($el, index)=>{
+// below one is for first table
+cy.get("table#product[name='courses']>tbody>tr>td:nth-child(2)").each(($el, index)=>{
     // get the text value of each tr td:nth-child(2)
     const data = $el.text()
-
-   
-
     if(data.includes("JMETER")){
         // next() -> points to the element next to it
-        
-
-        cy.get("tr td:nth-child(2)").eq(index).next().then(function(price)
+    cy.get("table#product[name='courses']>tbody>tr>td:nth-child(2)").eq(index).next().then(function(price)
         {
             const pricetext = price.text()
             // mocha assertion
--- cypress/e2e/demoMouseActions.cy.js
+++ cypress/e2e/demoMouseActions.cy.js
@@ -10,14 +10,25 @@ describe('Handling mouse hover action', () => {
       cy.get("#btn1").trigger("mouseover")
       cy.wait(3000)
       cy.get(".tooltip-inner").should('be.visible')
-      // cy.wait(3000)
-      // cy.visit("https://rahulshettyacademy.com/AutomationPractice/")
-      // cy.get("select#dropdown-class-example").click()
-      // cy.wait(3000)
-      // cy.get("#btn1").trigger("mouseup")
-      // cy.wait(3000)
-      // cy.get("#btn1").trigger("mousedown")
+      // if the value is not dynamic we can use like below
+      // cy.get("#btn1").should('have.attr','aria-describedby','tooltip732644')
 
+      //if the value is dynamic
+      cy.get("#btn1").should(($element) =>{
+        const storeattr = $element.attr('aria-describedby')
+        //mocha - chai assertion
+        expect(storeattr).to.contain('tooltip')
+        // expect(storeattr).to.match('tooltip')// create a regex and use it here for hte format sample - [(\tooltip\)(0-9)+]
+      })     
   
-  })
+  }),
+  it('Handling in mousehover in abnother website - this is failure if we did nto give force in click' , () =>{
+ cy.visit("https://rahulshettyacademy.com/AutomationPractice/")
+
+ // scrollIntoView() - keywiord in cypress where it wills croll till the element
+      cy.get("#mousehover").scrollIntoView()
+      cy.wait(2000)
+      cy.get("#mousehover").trigger('mouseover')
+      cy.get("a[href*=top]").click({force:true})
+    })
 })

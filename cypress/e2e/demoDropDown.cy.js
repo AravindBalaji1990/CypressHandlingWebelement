a13c56c2a46f20c23a11129dff6fd16b207befbd
AravindBalaji1990 <aravindbalaji.balaji7@gmail.com>
2024-07-06 10:18:37 +0300

    new func

--- cypress/e2e/demoDropDown.cy.js
+++ cypress/e2e/demoDropDown.cy.js
@@ -1,53 +1,43 @@
 /// <reference types="Cypress" />
 
 describe('Handling dropdwon values', () => {
-//   before(function() {
-//     cy.log('this is from before ')
-//     var env1 = Cypress.env("url1")
-        
-//         cy.visit(env1)
-//  })
-    it('Handling dropdwon values', () => {
+    it.only('Handling dropdwon values', () => {
      
         cy.visit("https://rahulshettyacademy.com/AutomationPractice/")
         // cy.visit("/")// this will ivoke the baseUrl keyword under the cypress.config.js file
-
-        
-
         // below i am trying to retrieve the value of the attribute value
         //select is a part of cypress keyword and it is used to select the items from the dropdown
 
         // i am using athe value attribute int he dropdwon element to select the partoicular item
         // select will check for the value attributes value
      cy.get('select[id=dropdown-class-example]').select('option1');
-
-     cy.wait(3000)
-
+     cy.wait(3000) // it does nothing but just wait for the timeperiod in  milliseconds to complete
      // select will check for the text linked to the particular tagname
      cy.get('select[id=dropdown-class-example]').select('Option2');
 
 
      // below is a way to select or identify the dropdown option based on the text available
      // any textbox or textfield if we want to gett he value fromt he field we use invoke("val") -> it will get the value attribute fromthe elment and check it
-      // cy.get("select[id=dropdown-class-example]").select('Option2').invoke("val").should("eq", "option2")
+      cy.get("select[id=dropdown-class-example]").select('Option2').invoke("val").should("eq", "option2")
 
 
 
       //children is akeyowr in cypress where it checks forthe elements child
       cy.get('select').children('option').should('have.length',4)// validate th total option
-      // cy.get('select').children('option').its('length').then((nooptions) =>{
-      //   cy.log('total items:', {nooptions})
-      // })
+      cy.get('select').children('option').its('length').then((nooptions) =>{
+        cy.log('total items:', {nooptions})
+      })
 
       
 
-      // cy.get('select#dropdown-class-example').invoke('text').then((dropdownValuesuserdefined) => {
-      //   cy.log("the value from dropdown : " + dropdownValuesuserdefined)
-      //   // /n select /n option1 /n option2 /n
+      cy.get('select#dropdown-class-example').invoke('text').then((dropdownValuesuserdefined) => {
+        cy.log("the value from dropdown : " + dropdownValuesuserdefined)
+        // /n select /n option1 /n option2 /n
+        //select|option1|option2|option3   -> end result
         
-      //   const valuesArray = dropdownValuesuserdefined.split('\n').map(value => value.trim()).filter(value => value !== '');
-      //   cy.log(`Dropdown Values: ${valuesArray.join('|')}`);
-      // });
+        const valuesArray = dropdownValuesuserdefined.split('\n').map(value => value.trim()).filter(value => value !== '');
+        cy.log(`Dropdown Values: ${valuesArray.join(',').includes("Select")}`);
+      });
 
     // to select multiple items in
     // cy.get('select#dropdown-class-example').select(["11", "12"]);
@@ -55,20 +45,20 @@ describe('Handling dropdwon values', () => {
     // find -> similar to the children inside the get what are the locator available
     // find will always go with get
 
-      // cy.get('select#dropdown-class-example').children('option').then(options => {
-      //   // ... spread operator 
-      //   // map -> do a action and create a new array of it
-      //   //[...options] -> this means ... means to add the elements in dynamic way which we call it as spread operator options is th eparameter we pass on the then it can be anything thata  user defines
-      //   const actualtext = [...options].map(o => o.text)
-      //   const actualvalue = [...options].map(o => o.value)
-      //   cy.log("the actual text :" + actualtext)
-      //   // mocha assertions - actual == expected
-      //   expect(actualvalue).to.deep.eq(['','option1','option2','option3'])
-      //   expect(actualtext).to.deep.eq(['Select','Option1','Option2','Option3'])
-      // })
+      cy.get('select#dropdown-class-example').children('option').then(options => {
+        // ... spread operator 
+        // map -> do a action and create a new array of it
+        //[...options] -> this means ... means to add the elements in dynamic way which we call it as spread operator options is th eparameter we pass on the then it can be anything thata  user defines
+        const actualtext = [...options].map(o => o.text)
+        const actualvalue = [...options].map(o => o.value)
+        cy.log("the actual text :" + actualtext)
+        // mocha assertions - actual == expected
+        expect(actualvalue).to.deep.eq(['','option1','option2','option3'])
+        expect(actualtext).to.deep.eq(['Select','Option1','Option2','Option3'])
+      })
 
   }),
-  it.only("how to get the default value in the dropdown",() =>{
+  it("how to get the default value in the dropdown",() =>{
     cy.visit("https://rahulshettyacademy.com/AutomationPractice/")
 
     cy.get("#dropdown-class-example").then($dropdown =>{
--- cypress/e2e/demoInvoke.cy.js
+++ cypress/e2e/demoInvoke.cy.js
@@ -1,7 +1,7 @@
 /// <reference types="Cypress" />
 
 describe('check for attribute and text', () => {
-    it.skip('check for attribute using invoke to get the attributes value', () => {
+    it('check for attribute using invoke to get the attributes value', () => {
      
       cy.visit('https://rahulshettyacademy.com/AutomationPractice/');
      
@@ -14,13 +14,55 @@ describe('check for attribute and text', () => {
   cy.get("#autocomplete").first().invoke('attr','placeholder').should('include','Countries')
   cy.get("#autocomplete").first().click()
   }),
-  it.only('check for attribute using invoke and get the text', () => {
+
+  it('check for attribute style using invoke to get the attributes value', () => {
+     
+    cy.visit('https://rahulshettyacademy.com/AutomationPractice/');
+   
+cy.get("#autocomplete").first().should('be.enabled')
+cy.get("#autocomplete").should('be.visible')
+// after giving a invoke we cannot do any aciton like click or type after that
+//invoke('attr','attributename') - whatever the attribute linked to the element will be picked up for validation
+cy.get("#autocomplete").first().type("India")
+
+cy.get(".ui-menu.ui-widget.ui-widget-content.ui-autocomplete.ui-front").invoke('attr','style').should('include','display: block')
+
+cy.get(".ui-menu.ui-widget.ui-widget-content.ui-autocomplete.ui-front>li").contains("India").click()
+}),
+
+  it('check for attribute using invoke and get the text', () => {
      
     cy.visit('https://rahulshettyacademy.com/AutomationPractice/');
    //last -is a keyword in cypress picks the last item fromthe css locator
    //invoke('text') - whatever the text linked to the element will be piocked up for validation
-cy.get(".right-align>fieldset>legend").last().should('be.visible')
-cy.get(".right-align>fieldset>legend").last().invoke('text').should('eq','Web Table Fixed header')
+// cy.get(".right-align>fieldset>legend").last().should('be.visible')
+// cy.get(".right-align>fieldset>legend").last().invoke('text').should('eq','Web Table Fixed header')
 // cy.get(".right-align>fieldset>legend").last().invoke('text').should('eq','Web Table Fixed header').click()// click cannot be applied after a invoke
+cy.get("#gf-BIG>table>tbody  tr>td>ul>li.gf-li:nth-child(2)").last().contains("Facebook")
+cy.get("#gf-BIG>table>tbody  tr>td>ul>li.gf-li:nth-child(2)").last().invoke('text').should('eq','Facebook')
+
+//if we need to reuse the text value at some point we can store it with 'as' - meaning aliase
+cy.get("#gf-BIG>table>tbody  tr>td>ul>li.gf-li:nth-child(2)").last().invoke('text').as("value")
+//we need to extract the content from the aliase name 
+cy.get('@value').then((xyz) =>{
+  cy.log("the value stored  ----> " , xyz)
+})
+
+
+}),
+it.only('aliase representaiton for re-usability', () => {
+     
+  cy.visit('https://rahulshettyacademy.com/AutomationPractice/');
+cy.get("#gf-BIG>table>tbody  tr>td>ul>li.gf-li:nth-child(2)").last().as("facebooklocator")
+cy.get("@facebooklocator").contains("Facebook")
+cy.get("@facebooklocator").invoke('text').should('eq','Facebook')
+//if we need to reuse the text value at some point we can store it with 'as' - meaning aliase
+cy.get("@facebooklocator").invoke('text').as("value")
+//we need to extract the content from the aliase name 
+cy.get('@value').then((xyz) =>{
+cy.log("the value stored  ----> " , xyz)
+})
+
+
 })
 })
